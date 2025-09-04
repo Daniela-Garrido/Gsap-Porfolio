fb6fd2db75411f809de2dacf7c481d10713caa45
Daniela Garrido <vj.nefertiti@gmial.com>
2025-09-04 01:26:08 -0400

    se agrega json y se mejoran todos los estilos y js , html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
+// Animaciones Letras
 const text = new SplitType(`.hero-title`, { types: 'words, chars', });
 
 text.chars.forEach(char => {
-
     let charsTl = gsap.timeline();
-
     gsap.from(char, {
         y: gsap.utils.random(-150, 150),
         x: gsap.utils.random(-150, 250),
@@ -11,18 +10,15 @@ text.chars.forEach(char => {
         scale: gsap.utils.random(0.5, 2),
         rotate: gsap.utils.random(-180, 180),
         ease: "back.out",
-
     })
     charsTl.from(char, {
         color: `rgb(${gsap.utils.random(0, 255)},${gsap.utils.random(0, 255)},${gsap.utils.random(0, 255)})`,
         duration: 1.8,
-
     })
 
     char.addEventListener("mouseenter", charsHover);
 
     function charsHover() {
-
         gsap.timeline()
             .to(char, {
                 y: gsap.utils.random(-70, 70),
@@ -50,70 +46,66 @@ text.chars.forEach(char => {
                 }
             })
     }
-
 })
 
+// Animaciones de scroll
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(`.titulo`, {
-    opacity:0,
-    scale:0,
-    duration:0.5,
-    scrollTrigger:".titulo",
+    opacity: 0,
+    scale: 0,
+    duration: 0.5,
+    scrollTrigger: ".titulo",
 })
 
 gsap.from(`.about-titulo`, {
-    opacity:0,
-    y:20,
-    delay:.8,
-    scrollTrigger:".about-titulo",
+    opacity: 0,
+    y: 20,
+    delay: .8,
+    scrollTrigger: ".about-titulo",
 })
 
 gsap.from(`.about-subtitulo span`, {
-    y:100,
-    duration:1,
-    stagger:0.2,
-    scrollTrigger:".about-subtitulo span",
+    y: 100,
+    duration: 1,
+    stagger: 0.2,
+    scrollTrigger: ".about-subtitulo span",
 })
 
 gsap.from(`.about-item`, {
-     y:100,
-    duration:1,
-    stagger:0.2,
-    scrollTrigger:".about-item",
-    delay:0.3,
-
+    y: 100,
+    duration: 1,
+    stagger: 0.2,
+    scrollTrigger: ".about-item",
+    delay: 0.3,
 })
 
 gsap.from(`.logo`, {
     x: -100,
     opacity: 0,
     duration: .5,
-
 })
 
 gsap.from(`.container`, {
     opacity: 0,
     duration: 1,
-    delay: 0.5,     
+    delay: 0.5,
     y: 50,
-    scrollTrigger:".container",        
-    
+    scrollTrigger: ".container",
 })
 
 gsap.from(`.imgloco`, {
     y: 100,
     opacity: 0,
     duration: 1,
-    delay: 0.5,     
-    scrollTrigger:".imgloco",
+    delay: 0.5,
+    scrollTrigger: ".imgloco",
 })
 
 gsap.from(`.prueba`, {
-
-y:100,
-opacity:0,
-duration:1,
-scrollTrigger:".prueba",
-
+    y: 100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: ".prueba",
 })
+
